7d3ef2b440948fe997211ef124a9f6bd17f53f12
Ray149s <rduenas2@csustan.edu>
2022-05-04 03:03:37 -0700

    updated with String

--- pythonday1.py
+++ pythonday1.py
@@ -18,3 +18,67 @@ print(5+5, "hey this is my faverate number while", 25+6, "is how old I am turnin
 # Variable
 age = 5
 print(age)
+
+# Legal Variable names
+"""
+You can use Underscore _ 
+You can start a variable with a Capital letter but you should not
+
+You can not use - 
+You can not start variable with a number
+you can not use key words like return 
+
+"""
+# import math
+"""
+result = 10/3
+print(math.floor(result))
+    will print the 3
+
+Print(math.ceil(result))
+    will print 4
+
+new_result= math.floor(result)
+
+print(new_result)
+    will print 3
+"""
+
+#Operators 
+"""
+///     / --> float division
+///    // --> integer division --> (math.floor)
+       % -->  mod
+            number = 345345
+            limit  = 10
+            print (number % limit)
+                will print 5
+        ** how to raise a power
+            print(3**5)
+                is 3^5
+        You can also use
+            import math
+            result = math.pow(3, 5)
+"""
+
+#String
+"""
+\n = new line
+\x = way to print hex character \x41 would print a 
+\" will allow you to print a "
+\\ allows you to escape the escape character
+
+Single quotes and double quotes work exactly the same way "Hello" = 'Hello'
+    you must pay attention to how your quotes are in your statements 
+        'Hello I hope you're doing well' 
+            This will throw error because we have three ' marks 
+Concatenation 
+    msg1= "hello "
+    msg2= "ray"
+    print(msg1 + msg2 + " Thats the way it works")
+    will print hello ray Thats the way it works
+
+How to brake up long strings umungs multiple lines
+    msg =( "hello this is a long string...."
+             "you see like I said its a long string")
+"""
