64f1942fdeccd7e4e605c0c97b5b83a6a169b98e
Ray149s <rduenas2@csustan.edu>
2022-05-04 02:29:37 -0700

    Additional work completed

--- pythonday1.py
+++ pythonday1.py
@@ -1,3 +1,20 @@
+
+#FIND OUT WHAT THE INDENTATION HIRARKEY IS WITH Python
+
+#Comments 
+    # is how you initiate comments everything after the # will be a comment there is no way to end comment midline with python
+    
+"""
+    This is a way to create comments you can do a multi line comment
+    It creates a string element that so its not proper comment but it works
+    
+"""
+
 print("hello world")
 xyz = "This is my practice program"
 print(xyz)
+print(5+5, "hey this is my faverate number while", 25+6, "is how old I am turning", end="\n")
+
+# Variable
+age = 5
+print(age)
